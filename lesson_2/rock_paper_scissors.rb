061887c7cf3e042cf8b653f4f8d8da00e590309d
narjoDev <63086420+narjoDev@users.noreply.github.com>
2023-07-30 11:06:01 -0700

    Refactor rock paper scissors after rubocop video

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -2,7 +2,7 @@
 # the computer makes a choice
 # the winner is displayed
 
-CHOICES = ['rock', 'paper', 'scissors']
+CHOICES = %w(rock paper scissors)
 
 def prompt(message)
   puts ">> #{message}"
@@ -21,10 +21,10 @@ def pick_computer_choice
   CHOICES.sample
 end
 
-def winner(a, b)
-  a_index = CHOICES.index(a)
-  b_index = CHOICES.index(b)
-  (a_index - b_index) % 3 # 0: tie, 1: a, 2: b
+def determine_winner(player_one, player_two)
+  one_index = CHOICES.index(player_one)
+  two_index = CHOICES.index(player_two)
+  (one_index - two_index) % 3 # 0: tie, 1: one, 2: two
 end
 
 def display_results(user_move, computer_move, winner_code)
@@ -42,7 +42,7 @@ def main
   loop do
     user_move = ask_user_choice
     computer_move = pick_computer_choice
-    winner_code = winner(user_move, computer_move)
+    winner_code = determine_winner(user_move, computer_move)
 
     display_results(user_move, computer_move, winner_code)
 
